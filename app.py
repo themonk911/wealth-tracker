d2cba19aca949d9c640349cb5a28c6500c0f708a
Mayank Daswani <themonk911@gmail.com>
2025-08-25 20:00:40 +0000

    Add proxy fix.

--- app.py
+++ app.py
@@ -5,8 +5,10 @@ from googleapiclient.errors import HttpError
 from collections import defaultdict
 from datetime import datetime
 from flask_caching import Cache
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
 cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 app.static_folder = 'static'
 
